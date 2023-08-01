e9c4d0724e4d5213bf28e034d9377e3456f0a837
ZakharTsarapov <tsarapovs@gmail.com>
2023-08-01 16:13:08 +0300

    work with hw-05

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -2,7 +2,9 @@ import express from "express";
 import authController from "../../controllers/auth-controller.js";
 import {validateBody} from "../../decorators/index.js";
 import usersSchemas from "../../Schemas/users-schemas.js";
-import {authenticate} from "../../middlewares/index.js";
+import {authenticate, upload} from "../../middlewares/index.js";
+import fs from "fs/promises";
+import path from "path";
 
 const authRouter = express.Router();
 
@@ -15,6 +17,15 @@ authRouter.get("/current", authenticate, authController.getCurrent);
 authRouter.post("/singout", authenticate, authController.singout);
 
 authRouter.patch("/users", authenticate, validateBody(usersSchemas.updateSubcriptionSchema), authController.updateSubscription);
+
+
+const AvatarDir = path.resolve("public", "avatars")
+authRouter.patch("/avatars", authenticate, upload.single("avatar", async(req, res) => {
+    const {path: tempUpload, originalname} = req.file;
+    const resultUpload = path.resolve(AvatarDir, originalname);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarUrl = path.resolve("public", "avatars", originalname);
+}))
 
 
 export default authRouter;
